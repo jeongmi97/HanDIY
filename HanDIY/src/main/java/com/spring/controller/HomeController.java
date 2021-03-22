1e31ea45353e844e14c050146d075851e559ae47
kjm7943 <kjm7943@naver.com>
2021-03-22 21:41:01 +0900

    mybatis 설정

--- HanDIY/src/main/java/com/spring/controller/HomeController.java
+++ HanDIY/src/main/java/com/spring/controller/HomeController.java
@@ -1,15 +1,23 @@
 package com.spring.controller;
 
+import java.sql.Connection;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import javax.inject.Inject;
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+
 
 /**
  * Handles requests for the application home page.
@@ -19,11 +27,20 @@ public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
+	@Inject
+	private SqlSessionFactory sql;
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
+		try {
+			SqlSession session = sql.openSession();
+			System.out.println("성공 : " + session);
+		} catch (Exception e) {
+			System.out.println("실패");
+			e.printStackTrace();
+		}
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
 		Date date = new Date();
